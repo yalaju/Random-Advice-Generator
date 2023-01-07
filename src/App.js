2acf0181e3727f4b3e0dc1e555281c1c2d479362
yalaju samuel <yalajusamuel8@gmail.com>
2023-01-07 12:10:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import fitstimg from "./images/pattern-divider-mobile.svg";
+import secondimg from "./images/icon-dice.svg";
 
 function App() {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(async () => {
+    let fetchData = async (url) => {
+      let response = await fetch(url);
+      let receivedData = await response.json();
+      console.log(receivedData);
+      setData(receivedData);
+      setLoading(false);
+    };
+    fetchData("https://api.adviceslip.com/advice");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <p>Loading.............</p>
+      ) : (
+        <div className="content">
+          <div className="ptagnum">
+            <p>ADVICE {data.slip.id}</p>
+          </div>
+          <div className="ptagcontent">
+            <p>"{data.slip.advice}"</p>
+          </div>
+          <div className="sec1img">
+            <img src={fitstimg} alt="" />
+          </div>
+          <div className="sec2img">
+            <img src={secondimg} alt="" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
